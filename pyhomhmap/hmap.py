f00b4a2b21aefe090ab15e53b4003dd7fa601854
HomeOnMars <159873500+HomeOnMars@users.noreply.github.com>
2024-07-23 23:17:37 +1000

    Added a func to plot the hmap

--- pyhomhmap/hmap.py
+++ pyhomhmap/hmap.py
@@ -18,6 +18,8 @@ from numpy import typing as npt
 #from scipy.interpolate import RegularGridInterpolator
 from scipy.ndimage import map_coordinates #, distance_transform_edt, gaussian_filter
 import png
+import matplotlib as mpl
+from matplotlib import pyplot as plt
 
 
 
@@ -45,12 +47,19 @@ class HMap:
         map width in meters (i.e. whatever unit self.data is in).
         It's 57344. for CSL2 world map, and 14336. for CSL2 playable area.
         (Because 57344 = 3.5*4*4096)
-        
-    
+
+    z_seabed : float
+        Seabed height in meters.
+        Defines the minimum height of self.data
+
+    z_sealvl : float
+        Sea level height in meters.
+        Defines the height of the ocean.
+        Every pixel in self.data below self.z_sealvl is considered in the sea.
 
         
     see self.__init__() and self.normalize() for details.
-    -------------------------------------------------------------------------------
+    ---------------------------------------------------------------------------
     """
 
     def __init__(
@@ -58,12 +67,16 @@ class HMap:
         data : npt.ArrayLike = np.zeros((4096, 4096), dtype=np.float64),
         # note: 57344 = 3.5*4*4096 is the CSL2 worldmap width
         map_width : float|tuple[float, float] = 57344.,
+        z_seabed : float = 64.,
+        z_sealvl : float = 128.,
     ):
         # variables
         
         self.data  : npt.NDArray[np.float64] = np.array(data, dtype=np.float64)
         # note: will normalize float into tuple of floats later
         self._map_width : tuple[float, float] = map_width
+        self.z_seabed = z_seabed
+        self.z_sealvl = z_sealvl
 
         
         # do things
@@ -103,6 +116,8 @@ class HMap:
         filename     : str,
         map_width    : float|tuple[float, float],    # 57344. wm / 14336. pa
         height_scale : float = 4096.,
+        z_seabed     : float = 64.,
+        z_sealvl     : float = 128.,
         verbose      : bool = True,
     ) -> Self:
         """Load height map from a png file.
@@ -116,17 +131,73 @@ class HMap:
             [x, y]-widths of the map in meters.
 
         height_scale: float
-            max height in meters storable in the data, i.e. the scale of height.
+            max height in meters storable in the data,
+            i.e. the scale of the height.
         
         """
-        npix_x, npix_y, pixels, meta = png.Reader(filename=filename).read_flat()
+        npix_x, npix_y, pixels, meta = png.Reader(
+            filename=filename).read_flat()
         bit_depth : int = meta['bitdepth']
         if verbose and bit_depth != 16:
             print(f"**  Warning: Unexpected {bit_depth = }")
         pixels = np.array(
             pixels).reshape( (npix_x, npix_y)
         ) * (height_scale / 2**bit_depth)
-        self.__init__(data=pixels, map_width=map_width)
+        self.__init__(
+            data      = pixels,
+            map_width = map_width,
+            z_seabed  = z_seabed,
+            z_sealvl  = z_sealvl,
+        )
         
         return self
-        
+
+
+
+    def plot(
+        self,
+        figsize : tuple[int, int] = (8, 6),
+        norm : None|str|mpl.colors.Normalize = 'default',
+        z_sealvl: None|float = None,
+    ) -> tuple[mpl.figure.Figure, mpl.axes.Axes]:
+        """Return a plot of the data.
+        """
+
+        # init
+        if z_sealvl is None:
+            z_sealvl = self.z_sealvl
+        if norm == 'default':
+            norm = mpl.colors.Normalize(vmin=self.z_seabed - z_sealvl)
+
+        # plot things
+        fig, ax = plt.subplots(figsize=figsize)
+        cax  = ax.imshow(self.data - z_sealvl, norm=norm)
+        cmap = fig.colorbar(cax)
+        ax.set_title(
+            "Height Map\n" +
+            f"(Seabed: {self.z_seabed:.0f}m above zero for self.data," +
+            f"{z_sealvl - self.z_seabed:.0f}m below sea)")
+        cmap.set_label('Meters above sea level')
+
+        # update tick labels
+        tick_locs = tuple([
+            np.linspace(0, self._npix_xy[i], 8, dtype=np.int64)
+            for i in range(2)
+        ])
+        tick_vals = tuple([
+            tick_locs[0] / self._npix_xy[0] * self._map_width[0],
+            (1. - tick_locs[1] / self._npix_xy[1]) * self._map_width[1],
+        ])
+        tick_labels = tuple([
+            [f'{tick_val:.0f}' for tick_val in tick_vals[i]]
+            for i in range(2)
+        ])
+        tick_labels[0][-1] = f"{    tick_labels[0][-1]}\nSE"
+        tick_labels[1][ 0] = f"NW\n{tick_labels[1][ 0]}"
+        tick_labels[1][-1] = f"{    tick_labels[1][-1]}\nSW"
+        ax.set_xticks(tick_locs[0])
+        ax.set_xticklabels(tick_labels[0])
+        ax.set_yticks(tick_locs[1])
+        ax.set_yticklabels(tick_labels[1])
+
+        return fig, ax
